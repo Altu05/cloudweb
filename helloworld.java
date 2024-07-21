5e939a914ce43c572153506a8f4e189eb3784700
Altu05 <er.altamash05@gmail.com>
2024-07-21 16:08:39 +0530

    Update helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+class Simple
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program in Batch 35");
+        System.out.println("In my DevOps class");
+    }
+};
